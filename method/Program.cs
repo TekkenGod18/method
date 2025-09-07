e4a096f96e487b69f5e1bcc78e8d15defc720f44
Administrator <admin@example.com>
2025-09-08 01:21:13 +0300

    0

--- method/Program.cs
+++ method/Program.cs
@@ -10,6 +10,22 @@ namespace method
     {
         static void Main(string[] args)
         {
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = Convert.ToInt32(Console.ReadLine());
+            CalcSum(a,b,c);
+            Console.ReadKey();
+        }
+       static void CalcSum(int a, int b)
+        {
+            int S = a + b;
+            Console.WriteLine(S);
+
+        }
+        static void CalcSum(int a, int b, int c)
+        {
+            int S = a + b + c;
+            Console.WriteLine(S);
         }
     }
 }
